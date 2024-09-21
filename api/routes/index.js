6c936f60f83c5ed089f50766671b376ad807762d
Aeerszl <aeerszl@gmail.com>
2024-09-21 17:51:04 +0300

    events ve Emitter dosyalrı eklendi #20

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var router = express.Router();
-// eslint-disable-next-line no-unused-vars
-const config = require('../config'); // config.js dosyasını import ettik.
 
 const fs = require("fs");
-// eslint-disable-next-line no-undef
+
 let routes = fs.readdirSync(__dirname);
+
 for (let route of routes) {
   if (route.includes(".js") && route != "index.js") {
-    router.use("/" +route.replace(".js",""), require('./' + route));
-    
+    router.use("/"+route.replace(".js", ""), require('./'+route));
   }
 }
 
